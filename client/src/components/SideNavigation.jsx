eeb4415b9ac0e86d53b032f86aff94e37966ec69
Nethu00 <nethunirajapakse@gmail.com>
2024-02-26 19:50:11 +0530

    Modified the top and side navi bars
    Modified the tab names to align with the PM's user interface. Adjusted the colors of the top and side navigation bars accordingly.

--- client/src/components/SideNavigation.jsx
+++ client/src/components/SideNavigation.jsx
@@ -9,6 +9,82 @@ import { FiMessageSquare, FiFolder, FiShoppingCart } from "react-icons/fi";
 
 const SideNavigation = () => {
   const menus = [
+    { name: "Dashboard", link: "/pmdashboard", icon: MdOutlineDashboard },
+    { name: "Pojects", link: "/projects", icon: AiOutlineUser },
+    { name: "Tasks", link: "/tasks", icon: FiMessageSquare },
+    {
+      name: "Progress",
+      link: "/progress",
+      icon: TbReportAnalytics,
+      margin: false,
+    },
+    { name: "Employees", link: "/", icon: FiFolder },
+  ];
+  const [open, setOpen] = useState(true);
+
+  return (
+    <div
+      className={`min-h-screen ${
+        open ? "w-72" : "w-16"
+      } duration-500 text-gray-100 px-4`}
+      style={{
+        background: "#101d3f",
+      }}
+    >
+      <div className="flex justify-end py-3">
+        <HiMenuAlt3
+          size={26}
+          className="cursor-pointer"
+          onClick={() => setOpen(!open)}
+        />
+      </div>
+      <div className="relative flex flex-col gap-4 mt-4">
+        {menus?.map((menu, i) => (
+          <Link
+            to={menu?.link}
+            key={i}
+            className={` ${
+              menu?.margin && "mt-5"
+            } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
+          >
+            <div>{React.createElement(menu?.icon, { size: "20" })}</div>
+            <h2
+              style={{
+                transitionDelay: `${i + 3}00ms`,
+              }}
+              className={`whitespace-pre duration-500 ${
+                !open && "opacity-0 translate-x-28 overflow-hidden"
+              }`}
+            >
+              {menu?.name}
+            </h2>
+            <h2
+              className={`${
+                open && "hidden"
+              } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit  `}
+            >
+              {menu?.name}
+            </h2>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SideNavigation;
+
+/*import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { MdOutlineDashboard } from "react-icons/md";
+import { RiSettings4Line } from "react-icons/ri";
+import { TbReportAnalytics } from "react-icons/tb";
+import { AiOutlineUser, AiOutlineHeart } from "react-icons/ai";
+import { FiMessageSquare, FiFolder, FiShoppingCart } from "react-icons/fi";
+
+const SideNavigation = () => {
+  const menus = [
     { name: "dashboard", link: "/", icon: MdOutlineDashboard },
     { name: "user", link: "/", icon: AiOutlineUser },
     { name: "messages", link: "/", icon: FiMessageSquare },
@@ -71,3 +147,4 @@ const SideNavigation = () => {
 };
 
 export default SideNavigation;
+*/
