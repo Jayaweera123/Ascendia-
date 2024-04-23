880e4db99acf62ce628e1d7e2f23340dfae9a9dc
Jayaweera123 <155569167+Jayaweera123@users.noreply.github.com>
2024-04-23 23:40:08 +0530

    merge with deshani

--- server/src/main/java/com/Ascendia/server/service/Project/impl/ProjectServiceImpl.java
+++ server/src/main/java/com/Ascendia/server/service/Project/impl/ProjectServiceImpl.java
@@ -20,7 +20,7 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public ProjectDto createProject(ProjectDto projectDto) {
-        Project project = ProjectMapper.mapToProject(projectDto);
+        Project project = ProjectMapper.mapProject(projectDto);
         project.setCreatedDate(LocalDate.now()); // set the createdDate here
         Project savedProject = projectRepository.save(project);
 
