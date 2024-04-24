8585433c4921b10f4d25832d042fb3523dea2d80
Nethu00 <nethunirajapakse@gmail.com>
2024-04-24 18:37:07 +0530

    Update ProjectDto.java

--- server/src/main/java/com/Ascendia/server/dto/Project/ProjectDto.java
+++ server/src/main/java/com/Ascendia/server/dto/Project/ProjectDto.java
@@ -1,9 +1,5 @@
 package com.Ascendia.server.dto.Project;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +15,11 @@ public class ProjectDto {
 
     private Long projectId;
     private String projectName;
-    private String pDiscription;
-    private String status;
+    private String projectType;
+    private String projectDescription;
+    private String projectStatus;
     private LocalDate createdDate;
+    private LocalDate endDate;
     private Long pmId;
     private String image;
 }
