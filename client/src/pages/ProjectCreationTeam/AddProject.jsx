f5dbe02414372ce1e032c2a011455f2244cd9fed
Jayaweera123 <155569167+Jayaweera123@users.noreply.github.com>
2024-04-25 20:31:53 +0530

    frontend

---
+++ client/src/pages/ProjectCreationTeam/AddProject.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import SideNavigation from "../../components/SideNavigation";
+import TopNavigation from "../../components/TopNavigation";
+
+/*
+function AddProject() {
+  return (
+    <>
+      <TopNavigation />
+      <section className="flex">
+        <SideNavigation />
+        <div className="ml-3.5 mt-10">
+       
+  
+        </div>
+
+
+
+        
+
+
+
+
+        <table className="min-w-full text-sm bg-white">
+                            <thead>
+                                <tr className="text-gray-700 border-b bg-blue-gray-100 border-blue-gray-50 border-y">
+                                <th scope="col" className="p-4"> </th>
+                                <th className="px-4 py-5 text-left">Equipment Code</th>
+                                <th className="px-4 py-5 text-left">Equipment Name</th>
+                                <th className="px-4 py-5 text-left">Quantity</th>
+                                <th className="px-4 py-5 text-left">Description</th>
+                                <th className="w-16 px-4 py-5 text-left"></th>
+                                <th className="w-16 px-4 py-5 text-left"></th>
+                               
+                                </tr>
+                            </thead>
+
+
+
+        
+      </section>
+    </>
+  );
+}
+
+export default AddProject;
+
+
+*/
+
+
