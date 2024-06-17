ec1d37ad35406eb209ea9660a6aa6031f1edcb15
deshanipalliyaguruge <deshanipalliyaguruge2000@gmail.com>
2024-06-17 22:20:59 +0530

    commit2

--- client/src/components/Store/dashboard/DashboardCards.jsx
+++ client/src/components/Store/dashboard/DashboardCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { listMaterial } from "../../../services/StoreServices";
 import { listEquipment } from "../../../services/StoreServices";
 
