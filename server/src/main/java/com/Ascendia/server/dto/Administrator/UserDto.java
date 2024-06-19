905a038942be93e3fba91e3eedfa13e3517f75ac
rashmiJayawardhana <rashmijayawardhana2001@gmail.com>
2024-06-20 01:48:34 +0530

    update

--- server/src/main/java/com/Ascendia/server/dto/Administrator/UserDto.java
+++ server/src/main/java/com/Ascendia/server/dto/Administrator/UserDto.java
@@ -15,7 +15,6 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
-
     private String firstName;
     private String lastName;
     private String designation;
