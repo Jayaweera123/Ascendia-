f0a632c268fefd5c689893e087c0f9a139bc77b9
Nethu00 <nethunirajapakse@gmail.com>
2024-06-21 11:35:11 +0530

    Update

--- client/src/pages/Admin/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import TopNavigationAdmin from "../../components/Admin/TopNavigationAdmin";
 import SideNavigationAdmin from "../../components/Admin/SideNavigationAdmin";
-import StatusGrid from "../../components/ProjectCreationTeam/StatusGrid";
-import TransactionChart from "../../components/ProjectCreationTeam/TransactionChart";
-import ProjectStatusChart from '../../components/ProjectCreationTeam/ProjectStatusChart';
+//import StatusGrid from "../../components/ProjectCreationTeam/StatusGrid";
+//import TransactionChart from "../../components/ProjectCreationTeam/TransactionChart";
+//import ProjectStatusChart from "../../components/ProjectCreationTeam/ProjectStatusChart";
 
 const DashBoard = () => {
   const [open, setOpen] = useState(true);
 
- 
   return (
     <div>
       <TopNavigationAdmin />
