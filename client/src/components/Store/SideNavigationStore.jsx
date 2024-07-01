0d53a9623dd1db626a0638a5407d4e88eb8d6dbb
deshanipalliyaguruge <deshanipalliyaguruge2000@gmail.com>
2024-07-01 08:57:44 +0530

    commit3

--- client/src/components/Store/SideNavigationStore.jsx
+++ client/src/components/Store/SideNavigationStore.jsx
@@ -20,7 +20,9 @@ const SideNavigationStore = () => {
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Yes, logout!',
-      cancelButtonText: 'Cancel'
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#001b5e',
+      cancelButtonColor: '#6b7280',
     }).then((result) => {
       if (result.isConfirmed) {
         AuthService.logout();
